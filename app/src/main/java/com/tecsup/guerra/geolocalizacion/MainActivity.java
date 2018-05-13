5dcef267f2a73155c00108bed992fe93d000f275
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-05-12 19:11:47 -0500

    error

--- app/src/main/java/com/tecsup/guerra/geolocalizacion/MainActivity.java
+++ app/src/main/java/com/tecsup/guerra/geolocalizacion/MainActivity.java
@@ -1,13 +1,226 @@
 package com.tecsup.guerra.geolocalizacion;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS,latitudeGPS;
+    double longitudeNetwork,latitudeNetwork;
+
+    TextView longitudeValueBest,latitudeValueBest;
+    TextView longitudeValueGPS,latitudeValueGPS;
+    TextView longitudeValueNetwork,latitudeValueNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+    longitudeValueBest=(TextView)findViewById(R.id.longitudeValueBest);
+    latitudeValueBest=(TextView)findViewById(R.id.latitudeValueBest);
+
+    longitudeValueGPS=(TextView)findViewById(R.id.longitudeValueGPS);
+    latitudeValueGPS=(TextView)findViewById(R.id.latitudeValueGPS);
+
+    longitudeValueNetwork=(TextView)findViewById(R.id.longitudeValueNetwork);
+    latitudeValueNetwork=(TextView)findViewById(R.id.latitudeValueNetwork);
+
     }
+
+    private boolean checkLocation() {
+        if (!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Su ubicación esta desactivada.\npor favor active su ubicación " +
+                        "usa esta app")
+                .setPositiveButton("Configuración de ubicación", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public void toggleGPSUpdate(View view) {
+        if (!checkLocation())
+            return;
+        Button button = (Button) view;
+        if (button.getText().equals(getResources().getString(R.string.pause))) {
+            locationManager.removeUpdates(locationListenerGPS);
+            button.setText(R.string.resume);
+        } else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            }
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 2 * 20 * 1000, 10, locationListenerGPS);
+            button.setText(R.string.pause);
+        }
+    }
+
+    public void toggleBestUpdates(View view) {
+        if (!checkLocation())
+            return;
+        Button button = (Button) view;
+        if (button.getText().equals(getResources().getString(R.string.pause))) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            }
+            locationManager.removeUpdates(locationListenerBest);
+            button.setText(R.string.resume);
+        } else {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+            criteria.setPowerRequirement(Criteria.POWER_LOW);
+            String provider = locationManager.getBestProvider(criteria, true);
+            if (provider != null) {
+                locationManager.requestLocationUpdates(provider, 2 * 20 * 1000, 10, locationListenerBest);
+                button.setText(R.string.pause);
+                Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void toggleNetworkUpdates(View view) {
+        if (!checkLocation())
+            return;
+        Button button = (Button) view;
+        if (button.getText().equals(getResources().getString(R.string.pause))) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            }
+            locationManager.removeUpdates(locationListenerNetwork);
+            button.setText(R.string.resume);
+        }
+        else {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 20 * 1000, 10, locationListenerNetwork);
+            Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
+            button.setText(R.string.pause);
+        }
+    }
+
+    private final LocationListener locationListenerBest = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeBest = location.getLongitude();
+            latitudeBest = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueBest.setText(longitudeBest + "");
+                    latitudeValueBest.setText(latitudeBest + "");
+                    Toast.makeText(MainActivity.this, "Best Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+    };
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueNetwork.setText(longitudeNetwork + "");
+                    latitudeValueNetwork.setText(latitudeNetwork + "");
+                    Toast.makeText(MainActivity.this, "Network Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitudeValueGPS.setText(longitudeGPS + "");
+                    latitudeValueGPS.setText(latitudeGPS + "");
+                    Toast.makeText(MainActivity.this, "GPS Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+    };
+
+
+
 }
